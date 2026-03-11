d70a9d6a0547054dc05ea654c8c2d0cbecc92eed
kurabachew Mengistu <kmengistu1216@gmail.com>
2026-03-11 13:11:10 -0500

    Use HashRouter for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {HashRouter} from 'react-router-dom';
 import App from './App.jsx';
 
 import "./assets/css/bootstrap.css";
@@ -10,7 +11,7 @@ import "./assets/css/styles.css";
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
 )
